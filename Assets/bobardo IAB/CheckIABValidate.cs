3beb3b0294314f6decdd70d910349b46e8385baa
Hojjat <hojjat.reyhane@yahoo.com>
2017-02-05 09:29:04 +0330

    Add project files

---
+++ Assets/bobardo IAB/CheckIABValidate.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using SimpleJSON;
+
+public class CheckIABValidate : MonoBehaviour {
+
+    public string packageName;
+    public string client_id;
+    public string client_secret;
+    public string refresh_token;
+
+    private Purchase currentPurchase;
+    private Action<bool, string, validateResult> callback;
+
+    public void check(Purchase purchase, Action<bool, string, validateResult> mCallback)
+    {
+        if(purchase == null)
+        {
+            callback(false, "purchase is null", null);
+            return;
+        }
+
+        currentPurchase = purchase;
+        callback = mCallback;
+
+        try
+        {
+            WWWForm form = new WWWForm();
+            form.AddField("grant_type", "refresh_token");
+            form.AddField("client_id", client_id);
+            form.AddField("client_secret", client_secret);
+            form.AddField("refresh_token", refresh_token);
+
+            WWW postRequest = new WWW("https://pardakht.cafebazaar.ir/devapi/v2/auth/token/", form);
+            StartCoroutine(waitForRefreshCode(postRequest));
+
+        }
+        catch
+        {
+            callback(false, "error requesting access code", null);
+        }
+    }
+
+    IEnumerator waitForRefreshCode(WWW www)
+    {
+        yield return www;
+
+        if (www.error == null)
+        {
+            string resultJSON = www.text;
+            JSONNode json = JSON.Parse(resultJSON);
+
+            String accessToken = json["access_token"].Value.ToString();
+
+            string checkURL = "https://pardakht.cafebazaar.ir/devapi/v2/api/validate/" + packageName + "/inapp/"
+                + currentPurchase.productId 
+                + "/purchases/" + currentPurchase.orderId 
+                + "/?access_token=" + accessToken;
+            WWW www1 = new WWW(checkURL);
+            StartCoroutine(waitForRequest(www1));
+        }
+        else
+        {
+            callback(false, "error getting access code", null);
+        }
+    }
+
+    IEnumerator waitForRequest(WWW www)
+    {
+        yield return www;
+        
+        if (www.error == null)
+        {
+            string resultJSON = www.text;
+            JSONNode json = JSON.Parse(resultJSON);
+
+            if (json["developerPayload"].Value.ToString() == currentPurchase.payload)
+            {
+                validateResult result = new validateResult();
+                result.isConsumed = json["consumptionState"].AsInt == 0;
+                result.isRefund = json["purchaseState"].AsInt == 1;
+                result.kind = json["kind"].Value.ToString();
+                result.payload = json["developerPayload"].Value.ToString();
+                result.time = json["purchaseTime"].Value.ToString();
+
+                callback(true, "purchase is valid", result);
+            }
+            else
+            {
+                callback(false, "error validating purchase. payload is not valid.", null);
+            }
+
+        }
+        else
+        {
+            callback(false, "error validating purchase. " + www.error, null);
+        }
+    }
+}
+
+public class validateResult
+{
+    public bool isConsumed;
+    public bool isRefund;
+    public string kind;
+    public string payload;
+    public string time;
+}
---
+++ Assets/bobardo IAB/InAppStore.cs
@@ -0,0 +1,240 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+/* Apache License. Copyright (C) Bobardo Studio - All Rights Reserved.
+ * Unauthorized publishing the plugin with different name is strictly prohibited.
+ * This plugin is free and no one has right to sell it to others.
+ * http://bobardo.com
+ * http://opensource.org/licenses/Apache-2.0
+ */
+
+[RequireComponent(typeof(StoreHandler))]
+public class InAppStore : MonoBehaviour
+{
+    public Product[] products;
+
+    public Text txtResult;
+    public Text coinTxt;
+    public GameObject successDialog, errorDialog, loadingDialog, retryDialog;
+    public Button btnDoubleCoin;
+    public Text txtSuccessDialog;
+    public Text txtErrorDialog;
+
+    private int coin = 0;
+    private bool doubleCoin;
+
+    private int selectedProductIndex;
+    private Purchase currentPurchase;
+
+    void Start()
+    {
+        coin = PlayerPrefs.GetInt("coin", 0);
+        coinTxt.text = coin + "$";
+        doubleCoin = (PlayerPrefs.GetInt("doubleCoin", 0) == 1);
+        btnDoubleCoin.interactable = !doubleCoin;
+    }
+
+    private void onPurchaseValidated(bool success, string message, validateResult result)
+    {
+        if (success)
+        {
+            if (!result.isRefund)
+            {
+                // you can check consumptionState
+                // if (result.isConsumed)
+
+                // you can give user the product
+                switch (selectedProductIndex)
+                {
+                    case 0: // 500 coin
+                        saveCoin(500);
+                        break;
+                    case 1: // enable double coin
+                        doubleCoin = true;
+                        PlayerPrefs.SetInt("doubleCoin", 1);
+                        btnDoubleCoin.interactable = false;
+                        break;
+                    default:
+                        throw new UnassignedReferenceException("you forgot to give user the product after purchase. product: " + currentPurchase.orderId);
+                }
+
+                txtSuccessDialog.text = "Thanks for purchasing.";
+                successDialog.SetActive(true);
+            }
+            else
+            {
+                Debug.Log("the purchase is refund");
+                txtErrorDialog.text = "the purchase is refund.";
+                errorDialog.SetActive(true);
+            }
+        }
+        else
+        {
+            // error in validating, or purchase is not valid
+            // you can let user retry validating the purchase
+            Debug.Log(message);
+            retryDialog.SetActive(true);
+        }
+
+        loadingDialog.SetActive(false);
+        txtResult.text = "validate result: " + success + ", " + message + "\n" + txtResult.text;
+    }
+
+    public void purchasedSuccessful(Purchase purchase)
+    {
+        txtResult.text = "purchased successful. product: " + purchase.productId + " , token: " + purchase.orderId + "\n" + txtResult.text;
+
+        currentPurchase = purchase;
+        GetComponent<CheckIABValidate>().check(purchase, onPurchaseValidated);
+
+    }
+
+    public void purchasedFailed(int errorCode, string info)
+    {
+        // purchase failed. show user the proper message
+        switch (errorCode)
+        {
+            case 1: // error connecting cafeBazaar
+            case 2: // error connecting cafeBazaar
+            case 4: // error connecting cafeBazaar
+            case 5: // error connecting cafeBazaar
+
+                break;
+            case 6: // user canceled the purchase
+
+                break;
+            case 7: // purchase failed
+
+                break;
+            case 8: // failed to consume product. but the purchase was successful.
+
+                break;
+            case 12: // error setup cafebazaar billing
+            case 13: // error setup cafebazaar billing
+            case 14: // error setup cafebazaar billing
+
+                break;
+            case 15: // you should enter your public key
+
+                break;
+            case 16: // unkown error happened
+
+                break;
+            case 17: // the result from cafeBazaar is not valid.
+
+                break;
+        }
+
+        loadingDialog.SetActive(false);
+
+        txtResult.text = "errorCode: " + errorCode + ", " + info + "\n" + txtResult.text;
+        txtErrorDialog.text = "operation failed." + "\n" + info;
+        errorDialog.SetActive(true);
+    }
+
+    public void userHasThisProduct(Purchase purchase)
+    {
+        // user already has this product
+        txtResult.text = "user has product: " + purchase.productId + " , token: " + purchase.orderId + "\n" + txtResult.text;
+
+        currentPurchase = purchase;
+        GetComponent<CheckIABValidate>().check(purchase, onPurchaseValidated);
+    }
+
+    public void failToGetUserInventory(int errorCode, string info)
+    {
+        // user has not this product or some error happened
+        switch (errorCode)
+        {
+            case 3:  // error connecting cafeBazaar
+            case 10: // error connecting cafeBazaar
+
+                break;
+            case 9: // user didn't login to cafeBazaar
+
+                break;
+            case 11: // user has not this product
+
+                break;
+            case 12: // error setup cafebazaar billing
+            case 13: // error setup cafebazaar billing
+            case 14: // error setup cafebazaar billing
+
+                break;
+            case 15: // you should enter your public key
+
+                break;
+            case 16: // unkown error happened
+
+                break;
+            case 17: // the result from cafeBazaar is not valid.
+
+                break;
+        }
+
+        loadingDialog.SetActive(false);
+
+        txtResult.text = "errorCode: " + errorCode + ", " + info + "\n" + txtResult.text;
+        txtErrorDialog.text = "operation failed." + "\n" + info;
+        errorDialog.SetActive(true);
+    }
+
+    public void retryValidatePurchase()
+    {
+        retryDialog.SetActive(false);
+        loadingDialog.SetActive(true);
+        GetComponent<CheckIABValidate>().check(currentPurchase, onPurchaseValidated);
+    }
+
+    public void purchaseProduct(int productIndex)
+    {
+        loadingDialog.SetActive(true);
+        selectedProductIndex = productIndex;
+        Product product = products[productIndex];
+        if (product.type == Product.ProductType.Consumable)
+        {
+            GetComponent<StoreHandler>().BuyAndConsume(product.productId);
+        }
+        else if (product.type == Product.ProductType.NonConsumable)
+        {
+            GetComponent<StoreHandler>().BuyProduct(product.productId);
+        }
+
+    }
+
+    public void checkIfUserHasProduct(int productIndex)
+    {
+        loadingDialog.SetActive(true);
+        selectedProductIndex = productIndex;
+        GetComponent<StoreHandler>().CheckInventory(products[productIndex].productId);
+    }
+    
+    private void saveCoin(int value)
+    {
+        coin = PlayerPrefs.GetInt("coin", 0);
+        PlayerPrefs.SetInt("coin", coin + value);
+
+        StartCoroutine(animateCoinsText(coin, value));
+    }
+
+    private IEnumerator animateCoinsText(int coin, int value)
+    {
+
+        float part = (float)value / 20;
+
+        for (int i = 0; i < 20; i++)
+        {
+            int extra = (int)((i + 1) * part);
+            coinTxt.text = coin + extra + "$";
+            yield return new WaitForSecondsRealtime(0.03f);
+        }
+    }
+
+    public void addFreeCoin(int value)
+    {
+        if (doubleCoin) value *= 2;
+        saveCoin(value);
+    }
+}
+
